ba633fa7eb9e02c88411afdf8a59c6d446e6a491
Rende11 <artamonovn@gmail.com>
2023-08-14 23:48:02 +0300

    Exercise 13

---
+++ src/htdp/chapter01/part02/ex13.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(define (string-first str)
+  (if (> (string-length str) 0)
+      (substring str 0 1)
+      ""))
+
+(provide string-first)
---
+++ test/htdp/ex13-test.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require rackunit
+         rackunit/text-ui
+         "../../src/htdp/chapter01/part02/ex13.rkt")
+
+(define base-test
+  (test-suite
+   "String first"
+   (test-case
+       "Hello"
+     (check-equal? (string-first "hello") "h"))))
+
+(run-tests base-test)
