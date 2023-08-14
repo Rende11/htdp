66613cab3e1da16d6dc356d8850f1132ebe476f1
Rende11 <artamonovn@gmail.com>
2023-08-15 00:21:23 +0300

    Exercise 20

--- src/htdp/chapter01/part02/ex20.rkt
+++ src/htdp/chapter01/part02/ex20.rkt
@@ -1,8 +1,10 @@
 #lang racket
 
 (define (string-delete str position)
-  (string-append
-   (substring str 0 position)
-   (substring str (add1 position))))
+  (if (> (string-length str) 0)
+      (string-append
+       (substring str 0 position)
+       (substring str (add1 position)))
+      ""))
 
 (provide string-delete)
--- test/htdp/ex20-test.rkt
+++ test/htdp/ex20-test.rkt
@@ -9,6 +9,9 @@
    "String delete"
    (test-case
        "Hello _World"
-     (check-equal? (string-delete "Hello _World" 5) "Hello_World"))))
+     (check-equal? (string-delete "Hello _World" 5) "Hello_World"))
+   (test-case
+       "Empty string"
+     (check-equal? (string-delete "" 0) ""))))
 
 (run-tests base-test)
